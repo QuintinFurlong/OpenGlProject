d96405e67f06b466da9663955d17d2f05cc7de1f
Quintin Furlong <C00218453@itcarlow.ie>
2017-12-14 13:08:17 +0000

    madness

--- SFMLOpenGL/Game.cpp
+++ SFMLOpenGL/Game.cpp
@@ -8,7 +8,7 @@ Game::~Game() {}
 
 void Game::run()
 {
-
+	srand(time(nullptr));
 	initialize();
 
 	Event event;
@@ -126,91 +126,295 @@ void Game::draw()
 {
 	cout << "Draw up" << endl;
 	glClear(GL_COLOR_BUFFER_BIT | GL_ACCUM_BUFFER_BIT);
-	glBegin(GL_POINTS); {
-		glVertex3f(-1.0, 1.0, -5.0);
+	glBegin(GL_POLYGON); {
+		for (float x = -2; x < 2; x += .01)
+		{
+			for (float y = -2; y < 2; y += .01)
+			{
+				glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
+				glVertex3f(x, y, -5.0);
+			}
+		}
 	}
+	window.display();
 	glEnd();
 	glBegin(GL_LINES); {
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-1.2, 1.0, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-1.2, 2.0, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-0.9, 2.0, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-0.9, 1.0, -5.0);
 	}
 	glEnd();
 	glBegin(GL_LINE_STRIP); {
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-0.7, 1.0, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-0.7, 2.0, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-0.3, 2.0, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-0.3, 1.0, -5.0);
 	}
 	glEnd();
 	glBegin(GL_LINE_LOOP); {
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(0.7, 1.0, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(0.7, 2.0, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(0.3, 2.0, -5.0);
 	}
 	glEnd();
 	glBegin(GL_TRIANGLES); {
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-2.0, 2.0, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-1.5, 2.0, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-2.0, 1.5, -5.0);
 	}
 	glEnd();
 	glBegin(GL_TRIANGLE_STRIP); {
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(1.5, 2.0, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(1.7, 2.0, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(1.3, 1.5, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(1.9, 1.5, -5.0);
 	}
 	glEnd();
 	glBegin(GL_TRIANGLE_FAN); {
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(0.0, 0.0, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(0.5, 0.0, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(0.0, 0.5, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-0.5, 0.0, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(0.0, -0.5, -5.0);
 	}
 	glEnd();
 	glBegin(GL_QUADS); {
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-2.0, 0.5, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-1.0, 0.5, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-1.0, -0.5, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-2.0, -0.5, -5.0);
 	}
 	glEnd();
 	glBegin(GL_QUAD_STRIP); {
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(1.75, 0.2, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(1.6, 0.2, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(1.6, 0.4, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(1.75, 0.4, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(1.45, 0.4, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(1.45, 0.2, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(1.6, 0.0, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(1.45, 0.0, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(1.6, -0.2, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(1.45, -0.2, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(1.6, -0.4, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(1.45, -0.4, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(1.3, -0.2, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(1.3, -0.4, -5.0);
 	}
 	glEnd();
 	glBegin(GL_POLYGON); {
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-0.2, -0.6, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(0.2, -0.6, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(0.6, -1, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(0.6, -1.4, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(0.2, -1.8, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-0.2, -1.8, -5.0);
+		glColor3f((rand() % 25) / 10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-0.6, -1.4, -5.0);
+		glColor3f((rand() % 25)/10, (rand() % 25) / 10, (rand() % 25) / 10);
 		glVertex3f(-0.6, -1, -5.0);
 	}
 	glEnd();
 
-	window.display();
+	
 }
 
 void Game::unload()
 {
 	cout << "Cleaning up" << endl;
 }
+
+//#include <Game.h>
+//
+// // Uncomment for Part 2
+// // ********************
+//bool flip = false;
+//int current = 1;
+// // ********************
+//
+////  Game::Game() : window(VideoMode(800, 600), "OpenGL"), primatives(2)
+//
+//Game::Game() : window(VideoMode(800, 600), "OpenGL"), primatives(2)
+//{
+//	 // Uncomment for Part 2
+//	 // ********************
+//	 index = glGenLists(primatives);
+//	 // ********************
+//}
+//
+//Game::~Game() {}
+//
+//void Game::run()
+//{
+//
+//	initialize();
+//
+//	Event event;
+//
+//	while (isRunning) {
+//
+//		cout << "Game running..." << endl;
+//
+//		while (window.pollEvent(event))
+//		{
+//			if (event.type == Event::Closed)
+//			{
+//				isRunning = false;
+//			}
+//		}
+//		update();
+//		draw();
+//	}
+//
+//}
+//
+//void Game::initialize()
+//{
+//	isRunning = true;
+//
+//	 // Uncomment for Part 2
+//	 // ********************
+//	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+//	glMatrixMode(GL_PROJECTION);
+//	glLoadIdentity();
+//	gluPerspective(45.0, window.getSize().x / window.getSize().y, 1.0, 500.0);
+//	glMatrixMode(GL_MODELVIEW);
+//
+//
+//	glNewList(index, GL_COMPILE);
+//	glBegin(GL_TRIANGLES);
+//	{
+//		glColor3f(0.0f, 0.0f, 1.0f);
+//		glVertex3f(0.0, 2.0, -5.0);
+//		glVertex3f(-2.0, -2.0, -5.0);
+//		glVertex3f(2.0, -2.0, -5.0);
+//	}
+//	glEnd();
+//	glEndList();
+//
+//	glNewList(index + 1, GL_COMPILE);
+//	glBegin(GL_TRIANGLES);
+//	{
+//		glColor3f(0.0f, 1.0f, 0.0f);
+//		glVertex3f(0.2, 0.0, -2.0);
+//		glVertex3f(-2.0, -2.0, -2.0);
+//		glVertex3f(2.0, -2.0, -2.0);
+//	}
+//	glEnd();
+//	glEndList();
+//
+//	 // Uncomment for Part 2
+//	 // ********************
+//}
+//
+//void Game::update()
+//{
+//
+//	 // Uncomment for Part 2
+//	 // ********************
+//	elapsed = clock.getElapsedTime();
+//
+//	if (elapsed.asSeconds() >= 1.0f)
+//	{
+//		clock.restart();
+//
+//		if (!flip)
+//		{
+//			flip = true;
+//			current++;
+//			if (current > primatives)
+//			{
+//				current = 1;
+//			}
+//		}
+//		else
+//			flip = false;
+//	}
+//
+//	if (flip)
+//	{
+//		rotationAngle += 0.005f;
+//
+//		if (rotationAngle > 360.0f)
+//		{
+//			rotationAngle -= 360.0f;
+//		}
+//	}
+//	 // ********************
+//
+//	cout << "Update up" << endl;
+//}
+//
+//void Game::draw()
+//{
+//
+//	 // Uncomment for Part 2
+//	 // ********************
+//	cout << "Draw up" << endl;
+//
+//	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//
+//	//Investigate Here!!!!!
+//
+//	cout << "Drawing Primative " << current << endl;
+//	glCallList(current);
+//	 // Uncomment for Part 2
+//	 // ********************
+//
+//	window.display();
+//
+//}
+//
+//void Game::unload()
+//{
+//	cout << "Cleaning up" << endl;
+//}
+//
